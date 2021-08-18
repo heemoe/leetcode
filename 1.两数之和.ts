d3f9fdd37954141e98ec9d15f4a0f28fc7d8ba2f
heemoe <zhonghx_wb@ec.cntaiping.com>
2021-08-18 13:57:52 +0800

    init

---
+++ 1.两数之和.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=typescript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+    if (map.has(diff)) {
+      return [map.get(diff), i];
+    }
+    map.set(nums[i], i);
+  }
+}
+// @lc code=end
---
+++ 9.回文数.ts
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=typescript
+ *
+ * [9] 回文数
+ */
+
+// @lc code=start
+function isPalindrome(x: number): boolean {
+  if (x < 0 || (x % 10 === 0 && x !== 0)) {
+    return false;
+  }
+  let revertedNumber: number = 0;
+  while (x > revertedNumber) {
+    revertedNumber = revertedNumber * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+
+  return x === revertedNumber || x === Math.floor(revertedNumber / 10);
+}
+// @lc code=end
