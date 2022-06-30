b0b5cca86760dafc678071401380495812aec709
ZHX <zhonghx@hydefi.io>
2022-06-30 11:30:00 +0800

    feat: update

---
+++ 1004.最大连续-1-的个数-iii.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1004 lang=java
+ *
+ * [1004] 最大连续1的个数 III
+ */
+
+// @lc code=start
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int n = nums.length;
+        int left = 0, lsum = 0, rsum = 0;
+        int ans = 0;
+        for (int right = 0; right < n; ++right) {
+            rsum += 1 - nums[right];
+            while (lsum < rsum - k) {
+                lsum += 1 - nums[left];
+                ++left;
+            }
+            ans = Math.max(ans, right - left + 1);
+        }
+        return ans;
+    }
+}
+// @lc code=end
