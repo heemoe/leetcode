66bc5625646a97322501007eb195f4af2b373174
heemoe <i@zsms.me>
2022-07-08 13:52:06 +0800

    update

--- 234.回文链表.java
+++ 234.回文链表.java
@@ -8,17 +8,40 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
+        ListNode reversed = reverse(head);
+        ListNode p1 = reversed;
+        ListNode p2 = head;
 
+        while (p1 != null &&
+                p1.next != null) {
+
+            if (p1.val != p2.val) {
+                return false;
+            }
+
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return true;
+    }
+
+    public ListNode reverse(ListNode node) {
+        if (node == null || node.next == null) {
+            return node;
+        }
+        ListNode curr = reverse(node.next);
+        node.next.next = node;
+        node.next = null;
+        return curr;
     }
 }
 // @lc code=end
-
